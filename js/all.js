3fb8094a629bccc7b41d401fac7697d4f50293ca
leia hsu <luv.hsu@gmail.com>
2018-12-14 16:28:43 +0800

    add scroll top

--- js/all.js
+++ js/all.js
@@ -1,12 +1,18 @@
-const loading = document.querySelector('.loading');
-window.addEventListener('load', function (event) {
-  setInterval(() => {
-    loading.className += " animated fadeOut";
-  }, 10000);
-});
+// const loading = document.querySelector('.loading');
+// window.addEventListener('load', function (event) {
+//   setInterval(() => {
+//     loading.className += " animated fadeOut";
+//   }, 10000);
+// });
 
 $(function () {
-
+  $.scrollify({
+    section: ".scroll__section",
+    sectionName: "section-name",
+    scrollSpeed: 1100,
+    overflowScroll: true,
+    setHeights: false
+  });
 });
 
 $('.grid').masonry({
