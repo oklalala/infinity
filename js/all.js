3cad79d18905f190081208c4f41bcf06b8e83376
Jimmy Chang <jack9462@gmail.com>
2018-12-15 21:32:27 +0800

    finish filter

--- js/all.js
+++ js/all.js
@@ -6,3 +6,25 @@ $(function () {
     var posTop = $(".view").scrollTop() - $('.container').offset().top
   });
 });
+
+$('.waterfall').masonry({
+  columnWidth: 350,
+  itemSelector: '.waterfall__item',
+  gutter: 50
+});
+
+//filter function
+$(function () {
+  var selectedClass = "";
+  $(".filter").click(function () {
+    selectedClass = $(this).attr("data-rel");
+    $(".waterfall").fadeTo(100, 0.1);
+    $(".waterfall div").not("." + selectedClass).fadeOut();
+    setTimeout(function () {
+      $("." + selectedClass).fadeIn();
+      $(".waterfall").fadeTo(300, 1);
+    }, 300);
+
+  });
+});
+
